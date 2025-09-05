cf6c57473e89d210f90e607abee9ea8aff7adad6
amuratakamitamu <keikei12261226@gmail.com>
2025-09-05 16:46:39 +0900

    bringup.launch.pyにjoy_nodeを追加し、ゲームコントローラーの入力を処理できるようにしました。

--- src/hairobo_bringup/launch/bringup.launch.py
+++ src/hairobo_bringup/launch/bringup.launch.py
@@ -5,6 +5,13 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
+            package='joy',
+            executable='joy_node',
+            name='joy_node',
+            output='screen',
+            parameters=[{'dev': '/dev/input/js0'}]
+        ),
+        Node(
             package='hairobo_controller',
             executable='dualshock4_node',
             name='dualshock4'
